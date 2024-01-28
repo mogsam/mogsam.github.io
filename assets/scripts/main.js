dfbdaa958cf9f231956e1d2fe26f6b1321274109
Lord-Sam Lamparero <lordsam8@yahoo.com>
2024-01-28 21:38:40 +0800

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,34 @@
+// const body = document.body;
+// // body.append("Hello World", "Bye!")
+
+// // const div = document.createElement("div")
+// // body.append(div)
+
+// // // div.innerHTML = "<strong>Hello World!</strong>"; // Display text to div with strong styling
+
+// // const strong = document.createElement("strong");
+// // strong.innerText = "Hello World!"
+// // div.append(strong);
+// const div = document.querySelector('div');
+
+// const spanHi = document.querySelector('#hi');
+// const spanBye = document.querySelector('#bye');
+
+// spanBye.remove();
+// div.append(spanBye);
+
+// console.log(spanHi.getAttribute("title"));
+// console.log(spanHi.setAttribute("id", "changed-id"));
+
+// console.log(spanHi.dataset);
+// spanHi.dataset.newVar= "hi"
+
+// spanHi.classList.add("hi3");
+// spanHi.classList.remove("hi1");
+// spanHi.classList.toggle("hi1"); // check if class is existing (will remove) or not (will put)
+
+
+// spanHi.style.color = "blue";
+// spanHi.style.backgroundColor = "yellow"
+
+// // CRC - container row column 
